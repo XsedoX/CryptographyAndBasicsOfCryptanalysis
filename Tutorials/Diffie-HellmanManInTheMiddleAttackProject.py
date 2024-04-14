6737cfd9233e9de8908c3bd7b8e7339bc2ca0e25
Juliusz Sadowski <sjulek6011@gmail.com>
2024-04-14 13:04:03 +0200

    Typo fix c->d

--- Tutorials/Diffie-HellmanManInTheMiddleAttackProject.py
+++ Tutorials/Diffie-HellmanManInTheMiddleAttackProject.py
@@ -8,23 +8,29 @@ def main():
     G = int(input("Provide second public number G = "))
     a = random.randint(1, 100)
     alices_public_key = G**a % P
-    fancy_print(f"Alice chooses random integer:\na = {a}")
+    fancy_print(f"Alice chooses random integer:\n"
+                f"a = {a}")
     input()
-    fancy_print(f"She also calculates her public key:\nA = G^a mod P = {G}^{a} mod {P} = {alices_public_key}")
+    fancy_print(f"She also calculates her public key:\n"
+                f"A = G^a mod P = {G}^{a} mod {P} = {alices_public_key}")
     input()
     b = random.randint(1, 100)
     bobs_public_key = G**b % P
-    fancy_print(f"Bob chooses random integer:\nb = {b}")
+    fancy_print(f"Bob chooses random integer:\n"
+                f"b = {b}")
     input()
-    fancy_print(f"He also calculates his public key:\nB = G^b mod P = {G}^{b} mod {P} = {bobs_public_key}")
+    fancy_print(f"He also calculates his public key:\n"
+                f"B = G^b mod P = {G}^{b} mod {P} = {bobs_public_key}")
     input()
     fancy_print("A and B are Alice's and Bob's public keys respectively. They can share them through public channel.")
     input()
     alices_secret = bobs_public_key**a % P
     bobs_secret = alices_public_key**b % P
-    fancy_print(f"To calculate shared secret number Alice needs to calculate:\ns = B ^ a mod P = {bobs_public_key}^{a} mod {P} = {alices_secret}")
+    fancy_print(f"To calculate shared secret number Alice needs to calculate:\n"
+                f"s = B ^ a mod P = {bobs_public_key}^{a} mod {P} = {alices_secret}")
     input()
-    fancy_print(f"Similarly Bob calculates the number:\ns = A ^ b mod P = {alices_public_key}^{b} mod {P} = {bobs_secret}")
+    fancy_print(f"Similarly Bob calculates the number:\n"
+                f"s = A ^ b mod P = {alices_public_key}^{b} mod {P} = {bobs_secret}")
     input()
     fancy_print("Now what if someone interrupts the exchange?\n")
     fancy_print("What if someone catches the numbers A and B?")
@@ -38,9 +44,16 @@ def main():
     fancy_print("He additionally swapped values of A and B with his owns, in a way that both Alice and Bob had no "
                 "idea about it.")
     input()
-    fancy_print(f"But he needs to prepare for it. He chooses 2 random numbers: \nc = {c}\nd = {d}\nfor Alice and Bob "
+    fancy_print(f"But he needs to prepare for it. He chooses 2 random numbers: \n"
+                f"c = {c}\n"
+                f"d = {d}\n"
+                f"for Alice and Bob "
                 f"respectively.\n")
-    fancy_print(f"Mallet also knows:\nP = {P},\nG = {G},\nA = {alices_public_key},\nB = {bobs_public_key}\n"
+    fancy_print(f"Mallet also knows:\n"
+                f"P = {P},\n"
+                f"G = {G},\n"
+                f"A = {alices_public_key},\n"
+                f"B = {bobs_public_key}\n"
                 f"because these are public numbers.")
     input()
     bobs_public_key_from_mallet = G**c % P
@@ -48,15 +61,18 @@ def main():
     fancy_print(f"Alice should get Bob's public key B = {bobs_public_key} but instead she got key that Mallet calculated:\n"
                 f"MB = G ^ c mod P = {G}^{c} mod {P} = {bobs_public_key_from_mallet}")
     input()
-    fancy_print(f"He does the same thing to Bob:\nMA = G ^ d mod P = {G}^{d} mod {P} = {alices_public_key_from_mallet}")
+    fancy_print(f"He does the same thing to Bob:\n"
+                f"MA = G ^ d mod P = {G}^{d} mod {P} = {alices_public_key_from_mallet}")
     input()
     fancy_print(f"Alice got Mallet's MB while being convinced that she has got Bob's public key B.\n")
     fancy_print("Bob got Mallet's MA while also being convinced that he has got Alice's public key A.")
     input()
     alice_secret_from_mallet = bobs_public_key_from_mallet**a % P
     bob_secret_from_mallet = alices_public_key_from_mallet**b % P
-    fancy_print(f"Now Alice calculates secret number \ns = MB ^ a mod P = {bobs_public_key_from_mallet}^{a} mod {P} = {alice_secret_from_mallet}\n")
-    fancy_print(f"Bob also calculates secret number \ns = MA ^ b mod P = {alices_public_key_from_mallet}^{b} mod {P} = {bob_secret_from_mallet}")
+    fancy_print(f"Now Alice calculates secret number \n"
+                f"s = MB ^ a mod P = {bobs_public_key_from_mallet}^{a} mod {P} = {alice_secret_from_mallet}\n")
+    fancy_print(f"Bob also calculates secret number \n"
+                f"s = MA ^ b mod P = {alices_public_key_from_mallet}^{b} mod {P} = {bob_secret_from_mallet}")
     input()
     alice_secret_mallet_has = alices_public_key**c % P
     bob_secret_mallet_has = bobs_public_key**d % P
